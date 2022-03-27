7889de27ed222a450b9ed9f373563fec0e388382
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2022-03-27 21:04:57 +0300

    WPF-LoginScreen
    MVVM

---
+++ OnlineExamSystem/ExamSystem/MVVM/View/LoginScreenLoginPanel.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamSystem.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for LoginScreenLoginPanel.xaml
+    /// </summary>
+    public partial class LoginScreenLoginPanel : UserControl
+    {
+        public LoginScreenLoginPanel()
+        {
+            InitializeComponent();
+            HiddenLoadingBar();
+        }
+        public void ShowLoadingBar()
+        {
+            LoadingBar.EnableCycling();
+            LoadingBar.Visibility = Visibility.Visible;
+        }
+        public void HiddenLoadingBar()
+        {
+            LoadingBar.StopCycling();
+            LoadingBar.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ OnlineExamSystem/ExamSystem/MVVM/ViewModel/LoginScreenLoginPanelViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamSystem.Core;
+namespace ExamSystem.MVVM.ViewModel
+{
+    public class LoginScreenLoginPanelViewModel : ObservableObject, ILocalizable
+    {
+        private ReadOnlyDictionary<string, string> _localizationMap;
+
+        public ReadOnlyDictionary<string, string> LocalizationMap
+        {
+            get => this._localizationMap;
+            set
+            {
+                this._localizationMap = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _loginButtonText;
+
+
+        public string LoginButtonText
+        {
+            get { return _loginButtonText; }
+            set { _loginButtonText = value; NotifyPropertyChanged(); }
+
+        }
+        private string _usernameTextBoxText;
+
+        public string UsernameTextBoxText
+        {
+            get { return _usernameTextBoxText; }
+            set
+            {
+                _usernameTextBoxText = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private string _passwordTextBoxText;
+
+        public string PasswordTextBoxText
+        {
+            get { return _passwordTextBoxText; }
+            set { _passwordTextBoxText = value;
+                NotifyPropertyChanged();
+
+            }
+        }
+        private string _createAccountText;
+
+        public string CreateAccountText
+        {
+            get { return _createAccountText; }
+            set { _createAccountText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        public LoginScreenLoginPanelViewModel()
+        {
+            _localizationMap = Localization.GetDefaultDictionary();
+             _loginButtonText = _localizationMap["LoginButtonText"];
+            _usernameTextBoxText = _localizationMap["UsernameTextBoxText"];
+            _passwordTextBoxText = _localizationMap["PasswordTextBoxText"];
+            _createAccountText = _localizationMap["CreateAccountText"];
+
+        }
+
+        public LoginScreenLoginPanelViewModel(ReadOnlyDictionary<string, string> localization)
+        {
+                _localizationMap = localization;
+        }
+       
+    }
+}
